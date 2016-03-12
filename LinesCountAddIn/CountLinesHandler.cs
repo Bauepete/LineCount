9d1e39ba22121f477c4ab72674f6b84dd2d0cb4f
Peter Bauer <peter.bauer@bajupa.com>
2016-03-12 21:55:47 +0100

    Added code to iterate over selected files.

--- LinesCountAddIn/CountLinesHandler.cs
+++ LinesCountAddIn/CountLinesHandler.cs
@@ -1,18 +1,44 @@
 ﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Projects;
 using System;
 
 namespace LinesCountAddIn
 {
     public class CountLinesHandler : CommandHandler
     {
+        private const string LINES_COUNT_DOCUMENT_NAME = "Lines Count Statistics";
+
         protected override void Update(CommandInfo info)
         {
+            object selectedItem = GetSelectedItem();
 
+            if (selectedItem == null)
+            {
+                info.Enabled = false;
+                return;
+            }
+
+            if (selectedItem is Project || selectedItem is Solution)
+                info.Enabled = true;
+            else
+            {
+                ProjectFile selectedFile = selectedItem as ProjectFile;
+                info.Enabled = selectedFile != null && LinesCountWriter.IsCSharpFile(selectedFile);
+            }
+        }
+
+        private static object GetSelectedItem()
+        {
+            return IdeApp.ProjectOperations.CurrentSelectedItem;
         }
 
         protected override void Run()
         {
-
+            LinesCountWriter w = new LinesCountWriter(LINES_COUNT_DOCUMENT_NAME);
+            object selectedItem = GetSelectedItem();
+            if (selectedItem != null)
+                w.WriteInfoOfSelectedItem(selectedItem);
         }
     }
 }
