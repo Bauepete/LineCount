a7cd3f7a47a4fe61399e6077b5a06689161cdd6c
Peter Bauer <peter.bauer@bajupa.com>
2016-03-16 16:45:32 +0100

    Test case: test first file implemented.

--- LinesCount/LinesCounter.cs
+++ LinesCount/LinesCounter.cs
@@ -5,28 +5,43 @@ namespace LinesCount
 {
     public class LinesCounter
     {
-        public class Result {
-            public struct OverallResult {
+        private ISourceLineAnalyzer sourceLineAnalyzer;
+
+        public class Result
+        {
+            public struct OverallResult
+            {
                 public int TotalLines;
                 public int SourceLines;
                 public int EffectiveLines;
                 public int CommentLines;
             }
-            public Result.OverallResult Overall { get;  set; }
-            public List<SourceFile> Details { get;  set; }
+
+            public Result.OverallResult Overall { get; set; }
+
+            public List<SourceFile> Details { get; set; }
         }
 
-        public Result Results { get; private set;}
+        public Result Results { get; private set; }
 
         public LinesCounter(ISourceLineAnalyzer sourceLineAnalyzer)
         {
+            this.sourceLineAnalyzer = sourceLineAnalyzer;
             Results = new Result();
             Results.Details = new List<SourceFile>();
         }
 
         public void Count(List<SourceFile> sourceFiles)
         {
+            Result.OverallResult r = Results.Overall;
 
+            sourceFiles[0].GetAnalyzed(sourceLineAnalyzer);
+            r.TotalLines = sourceFiles[0].LinesOfCode;
+            r.SourceLines = sourceFiles[0].SourceLinesOfCode;
+            r.EffectiveLines = sourceFiles[0].EffectiveLinesOfCode;
+            r.CommentLines = sourceFiles[0].CommentLines;
+
+            Results.Overall = r;
         }
 
 
--- LinesCounterTests/LinesCounterTests.cs
+++ LinesCounterTests/LinesCounterTests.cs
@@ -1,6 +1,7 @@
 ﻿using LinesCount;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace LinesCountTests
 {
@@ -15,6 +16,19 @@ namespace LinesCountTests
             Assert.AreEqual(0, lc.Results.Overall.EffectiveLines);
             Assert.AreEqual(0, lc.Results.Overall.CommentLines);
             Assert.AreEqual(0, lc.Results.Details.Count);
+        }
+
+        [Test]
+        public void TestFirstFile()
+        {
+            LinesCounter lc = new LinesCounter(new CSharpSourceLineAnalyzer());
+            SourceFile f1 = new SourceFile("f1.cs", new string[]{ "// f1.cs", "class F1", "{", "}" });
+
+            lc.Count(new List<SourceFile>(new SourceFile[]{f1}));
+            Assert.AreEqual(4, lc.Results.Overall.TotalLines);
+            Assert.AreEqual(3, lc.Results.Overall.SourceLines);
+            Assert.AreEqual(1, lc.Results.Overall.EffectiveLines);
+            Assert.AreEqual(1, lc.Results.Overall.CommentLines);
         }
     }
 }
